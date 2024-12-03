9e7fdf31a2d52d21d6493acc44d5740cc8183b3f
David <david.velasco@princeton.edu>
2024-12-03 09:34:37 -0500

    Added integral function to visualization functions

--- utils/visualization.py
+++ utils/visualization.py
@@ -19,6 +19,7 @@ def plot_field(s: Simulator,
                 dim: str = "z",
                 transpose: bool = True,
                 regular: bool = False,
+                integrate=False,
                 **kwargs):
     if regular:
         M = s.regular_mesh(M)
@@ -31,10 +32,17 @@ def plot_field(s: Simulator,
     elif s.ndim==3:
         x,y,z = s.regular_faces()
         if dim=="z":
-            plt.pcolormesh(x,y,M[var,M.shape[1]//2],**kwargs)
-        elif dim=="y":
-            plt.pcolormesh(x,z,M[var,:,M.shape[2]//2],**kwargs)
-            plt.colorbar()
+            axis=0
+        else:
+            axis=1
+            y=z
+        if integrate:
+            M=M[var].sum(axis=axis)/M.shape[axis+1]
+        else:
+            shape = M.shape[axis+1]//2
+            M=M[var,shape] if dim=="z" else M[var,:,shape]
+        plt.pcolormesh(x,y,M,**kwargs)
+        plt.colorbar()
     else:
         x = s.regular_centers()[0]
         plt.plot(x,M[var])
